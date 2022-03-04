5a8149897872f289bb389abfbb311c22e20ec68c
Kevin <kevin506059@gmail.com>
2022-03-04 11:00:36 +0800

    feat: add map array

--- game4.10/gamemap.h
+++ game4.10/gamemap.h
@@ -4,7 +4,9 @@ namespace game_framework {
 		bool isEmpty(int x, int y); // 判斷該格是0
 		bool isFull(int x, int y); // 判斷該格是1
 		void changeArray(int x, int y); // 陣列值改變
+        void init();
 	private:
-		int mapArray[10][6]; // 地圖邊界陣列
+		// 地圖邊界陣列
+		int mapArray[29][47];
 	};
 }
