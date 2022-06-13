4fa5106eab430f0bccb38f09cf69af7c7a05657a
Chung <kevin506059@gmail.com>
2022-06-14 01:44:52 +0800

    feat: level 4 map and diamond

---
+++ game4.10/Source/white-diamond.cpp
@@ -0,0 +1,17 @@
+﻿#include "stdafx.h"
+#include "Resource.h"
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "audio.h"
+#include "gamelib.h"
+#include "white-diamond.h"
+
+namespace game_framework {
+WhiteDiamond::WhiteDiamond()
+{
+}
+void WhiteDiamond::LoadBitmap()
+{
+	diamondPic.LoadBitmap(IDB_REDDIAMOND, RGB(255, 255, 255));
+}
+}
